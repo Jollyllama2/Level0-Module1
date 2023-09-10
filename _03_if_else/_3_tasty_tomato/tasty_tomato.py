f508a53e38f641ed24dc16c9e71da32555a6d670
ashwinsingh <ashwinjsingh012@gmail.com>
2023-09-10 16:30:11 -0700

    hu

--- _03_if_else/_3_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_3_tasty_tomato/tasty_tomato.py
@@ -1,23 +1,26 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import simpledialog
 
 window_width = 600
 window_height = 600
-
+if __name__ == '__main__':
+    window = Tk()
 root = tk.Tk()
 
 canvas = tk.Canvas(root, width=window_width, height=window_height, bg="#DDDDDD")
 canvas.grid()
 
 # 1. Ask the user what color tomato they would like and save their response
+tomato = simpledialog.askstring(title='hi', prompt="what color tomato would you like")
+
 #    You can give them up to three choices
-
-
+choices = simpledialog.askstring(title='hi',prompt='You have three choices; green red and yellow')
 # 2. Use if-else statements to draw the tomato in the color that they chose
 #    You can modify the code below or draw your own tomato
-canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
-canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+canvas.create_oval(95, 200, 500, 450, fill="red", outline="")
+canvas.create_oval(600, 260, 525, 450, fill="yellow", outline="")
 
-canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
+canvas.create_rectangle(25, 950, 35, 30, fill="green", outline="")
 
 root.mainloop()
--- _04_int/_2_simple_number_adder/simple_adder.py
+++ _04_int/_2_simple_number_adder/simple_adder.py
@@ -1,5 +1,10 @@
-"""
-* Write a Python program that asks the user for two numbers.
+import math
+from tkinter import messagebox, simpledialog, Tk
 
-* Display the sum of the two numbers to the user
-"""
+if __name__ == '__main__':
+    window = Tk()
+    from tkinter import messagebox, simpledialog, Tk
+    window.withdraw()
+# Write a Python program that asks the user for two numbers.
+numbers = simpledialog.askstring(title='hi',prompt='enter two numbers.')
+# Display the sum of the two numbers to the user
