77f70f159bb8e72399e20d3504faf5cd1b9bd766
ashwinsingh <ashwinjsingh012@gmail.com>
2023-09-13 18:28:27 -0700

    hu

--- _03_if_else/_3_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_3_tasty_tomato/tasty_tomato.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 
 window_width = 600
 window_height = 600
@@ -12,10 +12,12 @@ canvas = tk.Canvas(root, width=window_width, height=window_height, bg="#DDDDDD")
 canvas.grid()
 
 # 1. Ask the user what color tomato they would like and save their response
+choices = messagebox.showinfo(title='hi',message='You have three choic es; green red and yellow')
 tomato = simpledialog.askstring(title='hi', prompt="what color tomato would you like")
 
+
 #    You can give them up to three choices
-choices = simpledialog.askstring(title='hi',prompt='You have three choices; green red and yellow')
+
 # 2. Use if-else statements to draw the tomato in the color that they chose
 #    You can modify the code below or draw your own tomato
 canvas.create_oval(95, 200, 500, 450, fill="red", outline="")
