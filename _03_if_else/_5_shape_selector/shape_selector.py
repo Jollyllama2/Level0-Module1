66e96c0b7ae79e9c2c94ff102f8ea6cdb48061ac
ashwinsingh <ashwinjsingh012@gmail.com>
2023-09-24 12:55:36 -0700

    hu

--- _03_if_else/_5_shape_selector/shape_selector.py
+++ _03_if_else/_5_shape_selector/shape_selector.py
@@ -9,9 +9,20 @@ if __name__ == '__main__':
     window.withdraw()
     
     # Make a new turtle
-    
+    my_dog = turtle.Turtle()
     # Ask the user what shape they want to draw and store it in a variable
-    
+    shape = simpledialog.askstring(title="hi", prompt= 'what shape do you want to draw, square, circle, of triangle')
     # Draw the shape requested by the user using if-elif-else statements
-    
+    if shape == "circle":
+        my_dog.circle(radius=124, steps = 30)
+    if shape == "square":
+        for i in range(4):
+            my_dog.right(90)
+            my_dog.forward(100)
+    if shape == 'triangle':
+        for i in range(3):
+            my_dog.right(120)
+            my_dog.forward(100)
+
     # Call the turtle .done() method
+    turtle.done()
--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -1,13 +1,25 @@
-"""
-* Write a python program that asks the user a minimum of 3 riddles.
 
-* You can look at riddles.com if you don't already know any riddles.
+from tkinter import messagebox, simpledialog, Tk
 
-* Collect the response of each riddle from the user and compare their
-  answers to the correct answer. 
 
-* Use a variable to keep track of the correctly answered riddles
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+ # Write a python program that asks the user a minimum of 3 riddles.
+    riddle1 = simpledialog.askstring(title='hello',prompt='what do you call a grandfather clock?')
+    riddle2 = simpledialog.askstring(title='hi',prompt='what happens when you throw a blue rock in the red sea??????')
+    riddle3 = simpledialog.askstring(title='hi',prompt='what room has no windows')
+# You can look at riddles.com if you don't already know any riddles.
 
-* After all the riddles have been asked, tell the user how many they got
-  correct
-"""
+ #Collect the response of each riddle from the user and compare their
+  #answers to the correct answer.
+    if riddle1 == "an old timer":
+      messagebox.showinfo(title='hi',message='You get a point')
+    else:
+      messagebox.showinfo(title='hi',message='that is incorrect and you get no points')
+
+# Use a variable to keep track of the correctly answered riddles
+
+#After all the riddles have been asked, tell the user how many they got
+ # correct
+#"""
